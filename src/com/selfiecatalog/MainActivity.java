9c2aea889bc47a7ad9be02dc474fe1cb16ed15b5
772freed <772freed@gmail.com>
2014-03-04 22:32:35 +0200

    Camera Update 1.2
    update

--- src/com/selfiecatalog/MainActivity.java
+++ src/com/selfiecatalog/MainActivity.java
@@ -1,17 +1,21 @@
 package com.selfiecatalog;
 
+import java.io.File;
+
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0;
 
 	@Override
@@ -27,22 +31,31 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	static final String appDirectoryName = "SelfieCatalog";
+	static final File imageRoot = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), appDirectoryName);
+	
+	
 	public void gotoActivity(View v) {
-
+		
+	
+	
+		
 	//define the file-name to save photo taken by Camera activity
-	String fileName = "new-photo-name.jpg";
+	String fileName = "new-photo.jpg";
 	//create parameters for Intent with filename
 	ContentValues values = new ContentValues();
 	values.put(MediaStore.Images.Media.TITLE, fileName);
 	values.put(MediaStore.Images.Media.DESCRIPTION,"Image capture by camera");
 	//imageUri is the current activity attribute, define and save it for later usage (also in onSaveInstanceState)
-	Uri imageUri = getContentResolver().insert(
-	        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+	Uri imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);	
 	//create new Intent
 	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
 	intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
 		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+		
+		
 	}
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -57,6 +70,34 @@ public class MainActivity extends Activity {
 		    }
 		}
 		}
+	
+	public static File convertImageUriToFile (Uri imageUri, Activity activity)  {
+		Cursor cursor = null;
+		try {
+		    String [] proj={MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID, MediaStore.Images.ImageColumns.ORIENTATION};
+		    cursor = activity.managedQuery( imageUri,
+		            proj, // Which columns to return
+		            null,       // WHERE clause; which rows to return (all rows)
+		            null,       // WHERE clause selection arguments (none)
+		            null); // Order-by clause (ascending by name)
+		    int file_ColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+		    int orientation_ColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.ORIENTATION);
+		    if (cursor.moveToFirst()) {
+		        String orientation =  cursor.getString(orientation_ColumnIndex);
+		        return new File(cursor.getString(file_ColumnIndex));
+		    }
+		    return null;
+		} finally {
+		    if (cursor != null) {
+		        cursor.close();
+		    }
+		}
+		}
+	
+	
+	public void openGallery(View v) {
+
+	}
 	
 	
 }
