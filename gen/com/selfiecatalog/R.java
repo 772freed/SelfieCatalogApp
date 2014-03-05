a648ba21b64cd1a37d61abf343712ad62f3797f6
772freed <772freed@gmail.com>
2014-03-05 17:40:12 +0200

    ?

--- gen/com/selfiecatalog/R.java
+++ gen/com/selfiecatalog/R.java
@@ -10,6 +10,9 @@ package com.selfiecatalog;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int text_color=0x7f070000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -21,26 +24,27 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int button_gradient=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int btnIntend=0x7f080003;
-        public static final int btnIntendS=0x7f080004;
-        public static final int btnIntendV=0x7f080005;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f080001;
-        public static final int button3=0x7f080002;
-        public static final int imageView1=0x7f080006;
-        public static final int videoView1=0x7f080007;
+        public static final int action_settings=0x7f090008;
+        public static final int btnIntend=0x7f090003;
+        public static final int btnIntendS=0x7f090004;
+        public static final int btnIntendV=0x7f090005;
+        public static final int button1=0x7f090000;
+        public static final int button2=0x7f090001;
+        public static final int button3=0x7f090002;
+        public static final int imageView1=0x7f090006;
+        public static final int videoView1=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
