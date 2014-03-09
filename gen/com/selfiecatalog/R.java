a0d4d1dfa4c43679c3345d4c5d641a02c04ac731
772freed <772freed@gmail.com>
2014-03-09 15:21:20 +0200

    Pavel
    Updated design. Relative Layout changed to Linear Layout.

--- gen/com/selfiecatalog/R.java
+++ gen/com/selfiecatalog/R.java
@@ -44,8 +44,8 @@ public final class R {
         public static final int btnIntend=0x7f090005;
         public static final int btnIntendS=0x7f090006;
         public static final int btnIntendV=0x7f090007;
-        public static final int button1=0x7f090000;
-        public static final int button2=0x7f090001;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090000;
         public static final int button3=0x7f090002;
         public static final int gallery1=0x7f090004;
         public static final int imageView1=0x7f090003;
@@ -53,8 +53,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_main_menu=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
