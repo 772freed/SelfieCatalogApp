c18eda50a95f9806ac8855cfb05b5e87eca90bf3
772freed <772freed@gmail.com>
2014-03-10 08:51:41 +0200

    Pavel
    Recourse file for dimensions and background colors

--- gen/com/selfiecatalog/R.java
+++ gen/com/selfiecatalog/R.java
@@ -11,7 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int text_color=0x7f070000;
+        public static final int activity_main_back=0x7f040000;
+        public static final int horizontal_layout=0x7f040001;
+        public static final int text_color=0x7f040002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -20,8 +22,10 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int button_gap=0x7f050002;
+        public static final int buttons_layout_marginTop=0x7f050003;
     }
     public static final class drawable {
         public static final int antartica1=0x7f020000;
@@ -44,15 +48,15 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f09000a;
-        public static final int btnIntend=0x7f090006;
-        public static final int btnIntendS=0x7f090007;
-        public static final int btnIntendV=0x7f090008;
+        public static final int btnIntend=0x7f090005;
+        public static final int btnIntendS=0x7f090006;
+        public static final int btnIntendV=0x7f090007;
         public static final int button1=0x7f090001;
         public static final int button2=0x7f090000;
         public static final int button3=0x7f090002;
-        public static final int gallery1=0x7f090004;
-        public static final int imageView1=0x7f090003;
-        public static final int imgLogo=0x7f090005;
+        public static final int gallery1=0x7f090003;
+        public static final int imageView1=0x7f090008;
+        public static final int imgLogo=0x7f090004;
         public static final int videoView1=0x7f090009;
     }
     public static final class layout {
@@ -64,17 +68,17 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int album_name=0x7f05000a;
-        public static final int app_name=0x7f050000;
-        public static final int btnIntend=0x7f050006;
-        public static final int btnIntendS=0x7f050007;
-        public static final int btnIntendV=0x7f050008;
-        public static final int cannot=0x7f050009;
-        public static final int hello_world=0x7f050002;
-        public static final int take_photo=0x7f050003;
-        public static final int view_gallery=0x7f050004;
-        public static final int vote=0x7f050005;
+        public static final int action_settings=0x7f060001;
+        public static final int album_name=0x7f06000a;
+        public static final int app_name=0x7f060000;
+        public static final int btnIntend=0x7f060006;
+        public static final int btnIntendS=0x7f060007;
+        public static final int btnIntendV=0x7f060008;
+        public static final int cannot=0x7f060009;
+        public static final int hello_world=0x7f060002;
+        public static final int take_photo=0x7f060003;
+        public static final int view_gallery=0x7f060004;
+        public static final int vote=0x7f060005;
     }
     public static final class style {
         /** 
@@ -98,10 +102,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
