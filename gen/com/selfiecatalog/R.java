7de39091d9e017868255345c9f9f9d2e116759b0
772freed <772freed@gmail.com>
2014-03-11 09:01:04 +0200

    Pavel
    Icons changed

--- gen/com/selfiecatalog/R.java
+++ gen/com/selfiecatalog/R.java
@@ -50,25 +50,28 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int bg_main=0x7f02000a;
         public static final int button_gradient=0x7f02000b;
         public static final int camera=0x7f02000c;
-        public static final int gradient_background=0x7f02000d;
-        public static final int ic_launcher=0x7f02000e;
-        public static final int icon=0x7f02000f;
-        public static final int login_1=0x7f020010;
-        public static final int wwe_logo=0x7f020011;
+        public static final int camera_icon=0x7f02000d;
+        public static final int gallery_icon=0x7f02000e;
+        public static final int gradient_background=0x7f02000f;
+        public static final int ic_launcher=0x7f020010;
+        public static final int icon=0x7f020011;
+        public static final int login_1=0x7f020012;
+        public static final int vote_icon=0x7f020013;
+        public static final int wwe_logo=0x7f020014;
     }
     public static final class id {
         public static final int action_settings=0x7f09000c;
-        public static final int btnIntend=0x7f090008;
-        public static final int btnIntendS=0x7f090009;
-        public static final int btnIntendV=0x7f09000a;
+        public static final int btnIntend=0x7f090007;
+        public static final int btnIntendS=0x7f090008;
+        public static final int btnIntendV=0x7f090009;
         public static final int button1=0x7f090002;
         public static final int button2=0x7f090001;
         public static final int button3=0x7f090003;
         public static final int button4=0x7f090005;
-        public static final int gallery1=0x7f090006;
+        public static final int gallery1=0x7f090004;
         public static final int gridview=0x7f090000;
-        public static final int imageView1=0x7f090004;
-        public static final int imgLogo=0x7f090007;
+        public static final int imageView1=0x7f09000a;
+        public static final int imgLogo=0x7f090006;
         public static final int videoView1=0x7f09000b;
     }
     public static final class layout {
